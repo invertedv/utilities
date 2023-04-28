12c503edd3bc4857e190c3de03c2a29e3558c68a
will <will@invertedv.com>
2023-04-28 08:32:48 -0600

    adding funcs

--- utilities.go
+++ utilities.go
@@ -1,7 +1,9 @@
 package utilities
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"math/rand"
 	"os"
 	"strings"
@@ -9,6 +11,8 @@ import (
 	"github.com/invertedv/chutils"
 	"github.com/invertedv/keyval"
 )
+
+// ***************  Search
 
 // Position returns the index of needle in the haystack. It returns -1 if needle is not found.
 // If haystack has length 1, then it is split into a slice with delimiter ",".
@@ -46,6 +50,8 @@ func MaxInt(ints ...int) int {
 	return max
 }
 
+// ***************  Math
+
 // MinInt returns the minimum of ints
 func MinInt(ints ...int) int {
 	min := ints[0]
@@ -58,6 +64,8 @@ func MinInt(ints ...int) int {
 	return min
 }
 
+// ***************  Files
+
 // Slash adds a trailing slash if inStr doesn't end in a slash
 func Slash(inStr string) string {
 	if inStr[len(inStr)-1] == '/' {
@@ -65,6 +73,25 @@ func Slash(inStr string) string {
 	}
 
 	return inStr + "/"
+}
+
+// TempFile produces a random temp file name in the system's tmp location.
+// The file has extension "ext". The file name begins with "tmp" has length 3 + length.
+func TempFile(ext string, length int) string {
+	return Slash(os.TempDir()) + "tmp" + randomLetters(length) + "." + ext
+}
+
+// FileExists returns an error if "file" does not exist.
+func FileExists(file string) error {
+	// see if it is already there
+	_, err := os.Stat(file)
+	exists := !errors.Is(err, fs.ErrNotExist)
+
+	if !exists {
+		return fmt.Errorf("%s does not exist", file)
+	}
+
+	return nil
 }
 
 // randomLetters generates a string of length "length" by randomly choosing from a-z
@@ -80,6 +107,8 @@ func randomLetters(length int) string {
 	return name
 }
 
+// ***************  DB
+
 // TempTable produces a random table name. The table name begins with "tmp".
 // The table's name has length 3 +length.
 // tmpDB is the database name.
@@ -87,26 +116,25 @@ func TempTable(tmpDB string, length int) string {
 	return tmpDB + ".tmp" + randomLetters(length)
 }
 
-// TempFile produces a random temp file name in the system's tmp location.
-// The file has extension "ext". The file name begins with "tmp" has length 3 + length.
-func TempFile(ext string, length int) string {
-	return Slash(os.TempDir()) + "tmp" + randomLetters(length) + "." + ext
-}
+// TableOrQuery takes table and returns a query. If it is already a query (has "select"), just returns the original value.
+func TableOrQuery(table string) string {
+	switch {
+	case strings.Contains(strings.ToLower(table), "select"):
+		// make sure it's in parens
+		if table[0:1] != "(" {
+			table = fmt.Sprintf("(%s)", table)
+		}
 
-// FileExists returns an error if "file" does not exist.
-func FileExists(file string) error {
-	_, err := os.Open(file)
-	if err != nil {
-		return fmt.Errorf("file %s does not exist", file)
+		return table
+	default:
+		return fmt.Sprintf("(SELECT * FROM %s)", table)
 	}
-
-	return nil
 }
 
 // TableExists returns an error if "table" does not exist.
 // conn is the DB connector.
 func TableExists(table string, conn *chutils.Connect) error {
-	qry := fmt.Sprintf("SELECT * FROM %s LIMIT 1", table)
+	qry := fmt.Sprintf("SELECT * FROM %s LIMIT 1", TableOrQuery(table))
 	_, err := conn.Exec(qry)
 
 	if err != nil {
@@ -140,4 +168,12 @@ func BuildQuery(srcQry string, replacers keyval.KeyVal) (qry string) {
 	}
 
 	return qry
+}
+
+// DropTable drops the table from ClickHouse
+func DropTable(table string, conn *chutils.Connect) error {
+	qry := fmt.Sprintf("DROP TABLE %s", table)
+	_, err := conn.Exec(qry)
+
+	return err
 }
